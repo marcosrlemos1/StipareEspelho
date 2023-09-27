cc1d7a33f763c077cf11f6a2586d7234efb8cd07
Marcos Lemos <marcoslemos2002@outlook.com>
2023-09-27 11:02:06 -0300

    Nova IDE

---
+++ gui/pages/ui_pages.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'pagesKFkFzx.ui'
+##
+## Created by: Qt User Interface Compiler version 6.5.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from qt_core import *
+from gui.widgest.py_push_button import PyPushButton
+from gui.widgest.py_Slider import PySlider
+
+class Ui_StackedWidget(object):
+    def setupUi(self, StackedWidget):
+        if not StackedWidget.objectName():
+            StackedWidget.setObjectName(u"StackedWidget")
+        StackedWidget.setMaximumWidth(150)
+        StackedWidget.setMinimumWidth(150)
+
+        #CREAT PAGE1
+        self.page1 = QWidget()
+        self.page1.setObjectName(u"page1")
+        self.page1.setMinimumWidth(150)
+        self.page1.setMaximumWidth(150)
+
+        #CREATE LAYPUT FOR PAGE 1
+        self.verticalLayout = QVBoxLayout(self.page1)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0,0,0,0)
+
+        # LEFT MENU SPACING
+        self.left_menu_spacinggg = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+
+        self.label = QLabel("BRILHO")
+        self.verticalLayout.addWidget(self.label)
+        self.slider = PySlider()
+        self.verticalLayout.addWidget(self.slider)
+
+        self.label2 = QLabel("CONTRASTE")
+        self.verticalLayout.addWidget(self.label2)
+        self.slider2 = PySlider()
+        self.verticalLayout.addWidget(self.slider2)
+
+        self.label3 = QLabel("SELECIONE UMA OPÇÃO:")
+        self.verticalLayout.addWidget(self.label3)
+
+        self.combobox = QComboBox()
+        self.combobox.setStyleSheet("""
+                    QComboBox {
+                        background-color: #2B2D31;
+                        color: white;
+                        border: 1px solid #505050;
+                        border-radius: 4px;
+                        padding: 3px;
+                        font-size: 12px;
+                    }
+
+                    QComboBox QAbstractItemView {
+                        background-color: #2B2D31;
+                        border: 1px solid #505050;
+                        selection-background-color: #505050;
+                        color: white;
+                    }
+                """)
+        self.combobox.addItems(['Escala da super resolução', 'ESPCN_x2.pb', 'ESPCN_x3.pb', 'ESPCN_x4.pb'])
+        self.verticalLayout.addWidget(self.combobox)
+        self.verticalLayout.addItem(self.left_menu_spacinggg)
+
+
+        #CREATE PAGE 2
+        StackedWidget.addWidget(self.page1)
+        self.page2 = QWidget()
+        self.page2.setObjectName(u"page2")
+        self.page2.setMinimumWidth(150)
+        self.page2.setMaximumWidth(150)
+
+        #CREATE LAYOUT FOR PAGE2
+        self.verticalLayout_2 = QVBoxLayout(self.page2)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+
+        StackedWidget.addWidget(self.page2)
+        self.retranslateUi(StackedWidget)
+        QMetaObject.connectSlotsByName(StackedWidget)
+
+
+    def retranslateUi(self, StackedWidget):
+        StackedWidget.setWindowTitle(QCoreApplication.translate("StackedWidget", u"StackedWidget", None))
+    # retranslateUi
+
---
+++ main.py
@@ -0,0 +1,233 @@
+#IMPORT MODULES
+import sys
+import os
+
+#IMPORT QT_CORE
+from qt_core import *
+
+#import opencv
+import cv2
+
+#IMPORT MAIN WINDOW
+from gui.windows.main_window.ui_main_window import UI_MainWindow
+
+#class  MAIN WINDOW
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        #SETUP MAIN WINDOW
+        self.ui = UI_MainWindow()
+        self.ui.setup_ui(self)
+
+
+        #SHOW APPLICATION
+        self.show()
+        self.ui.toggle.setCheckable(False)
+        self.ui.toggle.clicked.connect(self.handle_toggle)
+
+        #INITIAL CAMERA PARAMETERS
+
+        self.brilho = 49
+        self.constraste = 49
+        self.selected_option = None
+        self.control = False
+
+        self.capture = None
+        self.camera_ativa = False
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.video_frame)
+        self.timer.timeout.connect(self.update_label)
+        self.timer.start(30) # Atualiza o frame a cada 30 milissegundos
+
+        self.ui.ui_pages.combobox.currentIndexChanged.connect(self.combobox_selection_changed)  # Conectar o sinal ao método
+        self.ui.ui_pages.slider.valueChanged.connect(self.slider_value_changed)
+        self.ui.ui_pages.slider2.valueChanged.connect(self.slider_value_changed2)
+
+        self.data_hora2 =self.update_label()
+
+        self.ui.toggle2.clicked.connect(self.show_page_2)
+        self.ui.toggle.clicked.connect(self.show_page_1)
+
+        self.ui.action1.triggered.connect(self.capture_and_save_image)
+        self.ui.action2.triggered.connect(self.exit_application)
+
+    def show_page_1(self):
+        self.ui.paginas.setCurrentWidget(self.ui.ui_pages.page1)
+
+    def show_page_2(self):
+        self.ui.paginas.setCurrentWidget(self.ui.ui_pages.page2)
+
+
+    def update_label(self):
+        atual = QDateTime.currentDateTime()
+        formatted_datetime = atual.toString("dd/MM/yyyy - hh:mm:ss")
+        self.ui.label_status.setText("STATUS: " + formatted_datetime)
+
+
+    #função responsavel para ativar e desativar a camera e botão
+    def handle_toggle(self):
+        #camera desativada e botão desativado
+        if self.ui.toggle.isChecked() and self.camera_ativa == True:
+            self.ui.toggle.setStyleSheet(
+                "QPushButton:hover {"
+                "background-color: #3E4145;" 
+                "}"
+                )
+            
+            self.capture.release()  # Libera a câmera
+            self.capture = None
+            self.camera_ativa = False
+        
+        #camera ativada e botão ativado
+        else:
+            self.ui.toggle.setCheckable(True)
+            self.camera_ativa = True
+            self.ui.toggle.setStyleSheet("background-color: red")
+            self.capture = cv2.VideoCapture(0)
+
+
+
+    def capturar_frame(self):
+        if self.capture is not None:
+            ret, self.frame = self.capture.read()  # Captura um frame da câmera
+            if ret:
+                self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)  # Converte para RGB
+                return cv2.resize(self.frame, (480, 360))
+            else:
+                QMessageBox.warning(None, "Sem frame", "A câmera não está retornando frame.")
+                self.capture = None
+                self.ui.toggle.setCheckable(True)
+
+    def video_frame(self):
+        self.frame = self.capturar_frame()
+        if self.frame is not None:
+            # Define as dimensões desejadas para exibição
+            target_width, target_height = 480, 360
+
+            # Redimensiona o quadro para as dimensões desejadas
+            resized_frame = cv2.resize(self.frame, (target_width, target_height))
+
+            # Converte o quadro redimensionado em uma imagem QImage
+            image = QImage(resized_frame.data, target_width, target_height, QImage.Format_RGB888)
+            pixmap = QPixmap.fromImage(image)
+
+            # Redimensiona a pixmap para a exibição sem cortar
+            pixmap = pixmap.scaled(target_width, target_height, Qt.AspectRatioMode.KeepAspectRatio)
+
+            self.ui.label_camera.setPixmap(pixmap)
+            self.ui.label_camera.setFixedSize(target_width, target_height)
+        else:
+            # Define as dimensões desejadas para exibição
+            target_width, target_height = 480, 360
+
+            # Cria uma imagem preta com as dimensões desejadas
+            dark_frame = QImage(target_width, target_height, QImage.Format_RGB888)
+            dark_frame.fill(Qt.black)
+
+            # Exibe o quadro escuro
+            pixmap = QPixmap.fromImage(dark_frame)
+
+            # Redimensiona a pixmap para a exibição sem cortar
+            pixmap = pixmap.scaled(target_width, target_height, Qt.AspectRatioMode.KeepAspectRatio)
+
+            self.ui.label_camera.setPixmap(pixmap)
+            self.ui.label_camera.setFixedSize(target_width, target_height)
+
+    def ajustar_brilho_contraste(imagem, brilho, contraste):
+        brilho = (((brilho + 1)-50)/50)*127
+        contraste = ((contraste + 1)*2)/100
+        # Aplicar a transformação linear de ajuste de brilho e contraste
+        imagem_ajustada = cv2.convertScaleAbs(imagem, alpha=contraste, beta=brilho)
+
+        return imagem_ajustada
+
+    # Aplicando super-resolução - ESPCN 
+    def super_resolucao(imagem, path, index):
+        sr = cv2.dnn_superres.DnnSuperResImpl_create()
+        sr.readModel(path)
+        sr.setModel("espcn", index+1)
+        result_sr = sr.upsample(imagem)
+
+        return result_sr
+    
+
+    #Função pra retornar o indice do Combobox
+    def combobox_selection_changed(self, index):
+        self.index = index
+        self.selected_option = 'ESPCN_SR/'+self.ui.ui_pages.combobox.currentText()  # Obter a opção selecionada
+        self.control = True
+        self.report(f"{self.data_hora()} / Escala de super resolução mudou para {self.ui.ui_pages.combobox.currentText()}")
+
+    #Função para retornar o valor do brilho
+    def slider_value_changed(self, value):
+        self.brilho = value
+        self.ui.ui_pages.label.setText(f"BRILHO : {(value+1)-50}")
+    
+    #Função para retornar o valor do contraste
+    def slider_value_changed2(self, value2):
+        self.constraste = value2
+        self.ui.ui_pages.label2.setText(f"CONTRASTE : {(value2+1)-50}")
+
+    #Função de tratamento de frame com correção de iluminação e super resolução
+    def tratamento_frame(self):
+        self.frame_tratado = cv2.imread("Foto.png")
+        self.frame_tratado = self.ajustar_brilho_contraste(self.frame_tratado, self.brilho, self.constraste)
+        if self.control is True:
+            self.frame_tratado = self.super_resolucao(self.frame_tratado, self.selected_option, self.index)
+            self.control = False
+        
+        # Define as dimensões desejadas para exibição
+        target_width, target_height = 420,280
+
+        # Redimensiona o quadro para as dimensões desejadas
+        imagem = cv2.resize(self.frame_tratado, (target_width, target_height))
+        imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
+        # Converte o quadro redimensionado em uma imagem QImage
+        image = QImage(imagem.data, target_width, target_height, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(image)
+
+        # Redimensiona a pixmap para a exibição sem cortar
+        pixmap = pixmap.scaled(target_width, target_height, Qt.AspectRatioMode.KeepAspectRatio)
+        self.ui.frame_1.setPixmap(pixmap)
+    
+    #Função de mensagem para o console 
+    def report(self, message):
+        # Adiciona uma mensagem de reportagem ao console
+        self.ui.text_edit.append(message)
+    
+    #Função para retornar a data e a hora atual
+    def data_hora(self):
+        atual = datetime.now()
+        return atual.strftime("%d/%m/%Y - %H:%M")
+    
+    def exit_application(self):
+        # Exibir um diálogo de confirmação antes de sair
+        reply = QMessageBox.question(self, 'Exit', 'Are you sure you want to exit?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            QApplication.quit()
+
+    def capture_and_save_image(self):
+
+        if self.camera_ativa == True:
+            # Capturar a tela inteira usando o OpenCV
+            screenshot = cv2.VideoCapture(0)
+            ret, frame = screenshot.read()
+
+            # Solicitar ao usuário um local e nome de arquivo para salvar a imagem
+            file_path, _ = QFileDialog.getSaveFileName(self, "Save Image", "", "PNG Files (*.png)")
+
+            # Salvar a imagem capturada como arquivo PNG usando o OpenCV
+            if file_path:
+                cv2.imwrite(file_path, frame)
+
+        else:
+            QMessageBox.warning(None,"sem frame", "A câmera não está ativa.")
+            
+
+if __name__=="__main__":
+    app = QApplication()
+    window = MainWindow()
+    sys.exit(app.exec())
